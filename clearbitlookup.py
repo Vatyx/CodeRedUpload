0f2f5b8f6ed81e0a58b60eea8aad831d6b6ab513
Vatyx <sahildhanju1@gmail.com>
2015-04-04 14:51:10 -0500

    fixed python

--- clearbitlookup.py
+++ clearbitlookup.py
@@ -1,9 +1,14 @@
+#!/usr/bin/python
 import sys
 import clearbit
-
+import cgi, cgitb
+import json
+#cgitb.enable()
 clearbit.key = '7ede27940236a369682ac4e7f411da85'
-url = sys.argv[1]
+url = cgi.FieldStorage()
+#url ="www.intel.com"
+print "Content-Type: text/html\n"
 
-company = clearbit.Company.find(domain=url, stream=True)
+company = clearbit.Company.find(domain=url["ourwebsite"].value, stream=True)
 if company != None:
   print json.dumps(company).encode("UTF-8")
