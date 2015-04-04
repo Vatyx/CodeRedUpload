5f2b99068d24434bdfd22509955920da614a4db6
bote795 <nbotell2@gmail.com>
2015-04-04 21:26:42 +0000

    changed to NEWS

--- newsFeedRetrival.py
+++ newsFeedRetrival.py
@@ -6,16 +6,16 @@ import urllib2
 import json
 keyBing="rUX6u17MhAbZ7jn7GmRKkucXRlh5ZxGqLWkZShIpbxY"
 credentialBing = 'Basic ' + (':%s' % keyBing).encode('base64')[:]
-searchString= cgi.FieldStorage()
-#searchString = '%27Xbox+One%27'
+#searchString= cgi.FieldStorage()
+searchString = '%27Xbox+One%27'
 top = 20
 offset = 0
-url = 'https://api.datamarket.azure.com/Bing/Search/Image?' + \
+url = 'https://api.datamarket.azure.com/Bing/Search/News?' + \
       'Query=%s&$top=%d&$skip=%d&$format=json' % (searchString, top, offset)
 request = urllib2.Request(url)
 request.add_header('Authorization', credentialBing)
 requestOpener = urllib2.build_opener()
 response = requestOpener.open(request) 
-#results = json.load(response)
+results = json.load(response)
 print "Content-Type: text/html\n"
-print response
+print results
