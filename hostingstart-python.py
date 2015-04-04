2c49a066a6fdc99fc863c05a123802ca2d95642f
Vatyx <sahildhanju1@gmail.com>
2015-04-04 01:38:01 -0500

    index 2

--- hostingstart-python.py
+++ hostingstart-python.py
@@ -3,6 +3,6 @@ import platform
 
 def application(environ, start_response):
     start_response(b'200 OK', [(b'Content-Type', b'text/html')])
-    with open ("index.html", "r") as hostingstart_file:
+    with open ("index2.html", "r") as hostingstart_file:
         hosting = hostingstart_file.read()
         yield hosting.encode('utf8').replace(b'PYTHON_VERSION', platform.python_version().encode('utf8'))
